0db73151fe3aeaceeff6fdf19253591e649e2b96
Hamada <your-email-address@domain.com>
2024-03-05 09:27:37 +0100

    port to latest openai api

--- azure_openai.py
+++ azure_openai.py
@@ -1,13 +1,12 @@
 from dotenv import load_dotenv
 import os
-import openai
+from openai import AzureOpenAI
 
 load_dotenv()
 
-openai.api_type = "azure"
-openai.api_base = "https://azureopenai-vastmindz.openai.azure.com/"
-openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = AzureOpenAI(azure_endpoint=os.getenv("OPENAI_API_ENDPOINT"),
+api_version="2024-02-15-preview",
+api_key=os.getenv("OPENAI_API_KEY"))
 
 def get_completion_from_messages(system_message, user_message, model="gpt-4", temperature=0, max_tokens=500) -> str:
 
@@ -16,14 +15,17 @@ def get_completion_from_messages(system_message, user_message, model="gpt-4", te
         {'role': 'user', 'content': f"{user_message}"}
     ]
     
-    response = openai.ChatCompletion.create(
-        engine=model,
-        messages=messages,
-        temperature=temperature, 
-        max_tokens=max_tokens, 
-    )
+    response = client.chat.completions.create(model=model,
+    messages=messages,
+    temperature=temperature, 
+    max_tokens=max_tokens)
     
-    return response.choices[0].message["content"]
+    result =  response.choices[0].message.content
+
+    # Hack to format the result
+    result = result.replace('```json', '').replace('```', '')
+
+    return result
 
 if __name__ == "__main__":
     system_message = "You are a helpful assistant"
